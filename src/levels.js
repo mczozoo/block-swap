a85278f34d4434a498334628e95bc4e8d74c0505
Zoltán Megyeri <zoltan.megyeri@funfactive.com>
2025-10-17 19:14:40 +0200

    Adjust level progression difficulty

--- src/levels.js
+++ src/levels.js
@@ -25,33 +25,47 @@ function createScrambled(size, swaps) {
 export const LEVELS = [
   {
     id: 1,
-    size: 3,
-    target: createTarget(3),
-    scrambled: createScrambled(3, [
+    size: 2,
+    target: createTarget(2),
+    scrambled: createScrambled(2, [
       [0, 1],
-      [3, 4],
-      [6, 7],
+      [2, 3],
     ]),
   },
   {
     id: 2,
-    size: 4,
-    target: createTarget(4),
-    scrambled: createScrambled(4, [
-      [0, 5],
-      [3, 12],
-      [6, 9],
+    size: 3,
+    target: createTarget(3),
+    scrambled: createScrambled(3, [
+      [0, 1],
+      [4, 5],
+      [7, 8],
+      [2, 4],
     ]),
   },
   {
     id: 3,
+    size: 4,
+    target: createTarget(4),
+    scrambled: createScrambled(4, [
+      [0, 5],
+      [1, 4],
+      [2, 10],
+      [6, 9],
+      [7, 13],
+    ]),
+  },
+  {
+    id: 4,
     size: 5,
     target: createTarget(5),
     scrambled: createScrambled(5, [
       [0, 24],
-      [6, 7],
-      [12, 13],
-      [18, 19],
+      [1, 6],
+      [5, 10],
+      [7, 17],
+      [12, 18],
+      [19, 23],
     ]),
   },
 ];
